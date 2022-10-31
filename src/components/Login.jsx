d9e5c7a92d8de5e8a4270d2b5d2cbc303ff00710
Chris Sanchez <christian.valenzuela@correounivalle.edu.co>
2022-10-31 00:20:16 -0500

    update structure and add components Login and PreLogin

---
+++ src/components/Login.jsx
@@ -0,0 +1,73 @@
+import './styles/loginRegistration.css';
+import AuthContext from '../services/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { useContext, useRef } from "react";
+import backArrow from '../assets/icons/backArrow.png';
+
+function Login() {
+	const email = useRef("");
+	const password = useRef("");
+	const { login } = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	const handleBack = (event) => {
+		event.preventDefault();
+		navigate("/");
+	}
+
+	const loginSubmit = async (event) => {
+		event.preventDefault();
+		console.log(email.current.value)
+		console.log(password.current.value)
+		const data = {
+			email: email.current.value,
+			password: password.current.value,
+		};
+		await login(data);
+	};
+
+	return (
+		<div>
+			<button
+				className='backArrow'
+				title='Volver'
+				onClick={handleBack}>
+				<img src={backArrow} />
+			</button>
+			<div className='spaceAboveLoginTittle'></div>
+			<h1 className='tittle'>Inicia sesión</h1>
+			<br />
+			<form className='loginForm'>
+				<input
+					className='textField'
+					type='email'
+					ref={email}
+					placeholder='Correo electrónico'
+					required=''
+				/>
+				<div className='spaceForEmailPasswordAndLogInButton'></div>
+				<input
+					className='textField'
+					type='password'
+					ref={password}
+					placeholder='Contraseña'
+					required=''
+				/>
+				<div className='spaceForEmailPasswordAndLogInButton'></div>
+				<input
+					title='Iniciar sesión'
+					className='logInButton'
+					type='submit'
+					value='Iniciar sesión'
+					onClick={loginSubmit}
+				/>
+				<div className='spaceForEmailPasswordAndLogInButton'></div>
+				<span id='error' className='errorMessage'>
+					{/* Here you may get an error message like "Datos incorrectos, intente nuevamente" */}
+				</span>
+			</form>
+		</div>
+	);
+}
+
+export default Login;
---
+++ src/components/PreLogin.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import './styles/preLogin.css';
+
+export const PreLogin = () => {
+	const navigate = useNavigate();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		navigate("/login");
+	}
+	return (
+		<div className='base'>
+			<h1> Uni-Drive</h1>
+			<h2>¡Hola!, selecciona tu tipo de cuenta:</h2>
+			<div>
+				<button
+					className='buttonPasajero'
+					onClick={handleSubmit}>
+					Pasajero </button>
+				<button className='buttonOfertante'
+					onClick={handleSubmit}>
+					Ofertante </button>
+			</div>
+
+			<footer>
+				<a className='tienesCuenta'>¿No tienes una cuenta?</a>
+				<a className='registrate'>Registrate</a>
+			</footer>
+		</div>
+	);
+};
