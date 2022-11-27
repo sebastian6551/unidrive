a35920f11acbb97c8221d9ae9c675727ab8869b1
Sara Eraso <sara.eraso@correounivalle.edu.co>
2022-11-27 18:00:25 -0500

    cardComponent modified with props, redBoxInterfaceTitleComponent added

--- src/components/redBoxInterfaceTitleComponent.jsx
+++ src/components/redBoxInterfaceTitleComponent.jsx
@@ -1,13 +1,33 @@
-import {
-	Typography,
-	Grid,
-	Paper,
-} from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 
-export const redBoxInterfaceTitleComponent = () => {
+export const RedBoxInterfaceTitleComponent = props => {
 	return (
-
-        <div></div>
-
+		<Paper
+			textAlign='left'
+			sx={{
+				p: 2,
+				margin: 'auto',
+				maxWidth: 500,
+				flexGrow: 1,
+				width: 305,
+				height: 49,
+				background: '#D83C3E',
+				borderRadius: 3,
+			}}
+		>
+			<Typography
+				textAlign='left'
+				sx={{					
+					fontFamily: 'jost',
+					fontStyle: 'normal',
+					fontSize: 25,
+					fontWeight: 400,
+					color: '#FFFFFF',				
+				}}
+			>
+				{' '}
+				{props.title}{' '}
+			</Typography>
+		</Paper>
 	);
 };
