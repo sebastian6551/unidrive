2198733775e8733a6572c9cf274d7adbe901506d
sara_maria <sara.eraso@correounivalle.edu.co>
2022-11-28 15:57:39 -0500

    travelInfo component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import { DashBoard } from "./components/DashBoard";
 import { SingUp } from "./pages/Singup"
 import Login from "./components/Login"
-import { PreLogin } from './components/PreLogin';
+// import { PreLogin } from './components/PreLogin';
 import { ProtectedRoute } from "./services/ProtectedRoutes.jsx";
 import { AuthContextProvider } from "./services/AuthContext";
 import {HomeBidder} from "./pages/HomeBidder";
 import {HomeRider} from "./pages/HomeRider";
+import { TravelInformationComponent} from './components/TravelInformationComponent';
 
 function App() {
 	return (
 		<AuthContextProvider>
 			<Routes>
-				<Route path="/" element={<PreLogin/>}></Route>
+				<Route path="/" element={<TravelInformationComponent typeOfTransport="tipo de transporte"/>}></Route>
 				<Route
 					path="/login"
 					element={
