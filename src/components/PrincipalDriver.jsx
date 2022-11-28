8bb793c6001abfc32e683776e73c4d09234eadf2
sebastian6551 <sebastian6551@live.com>
2022-11-27 23:40:46 -0500

    Driver home page

---
+++ src/components/PrincipalDriver.jsx
@@ -0,0 +1,35 @@
+import './styles/principalDriver.css';
+import './styles/principalBidder.css';
+import AuthContext from '../services/AuthContext';
+import { useContext } from 'react';
+import { AppBarComponent } from './AppBarComponent';
+import logOutArrow from '../assets/icons/logOutArrow.png';
+
+export const PrincipalDriver = () => {
+	const { logout } = useContext(AuthContext);
+	return (
+		<div>
+			<div className='space2vh'></div>
+			<span className='spaceLeft'>
+				<button
+					className='logOutArrow'
+					title='Cerrar sesión'
+					type='button'
+					onClick={logout}
+				>
+					<img src={logOutArrow} />
+				</button>
+			</span>
+			<div className='space9px'></div>
+			<button className='searchTravelButton' title='Crear viaje' type='button'>
+				Crear viaje +
+			</button>
+			<div className='space9px'></div>
+			<div className='inCourseContainer'></div>
+			<div className='space9px'></div>
+			<div className='appBarPosition'>
+				<AppBarComponent></AppBarComponent>
+			</div>
+		</div>
+	);
+};
