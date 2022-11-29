cecfaeed663fb987ae4f8402fb908edfea3a0f08
sebastian6551 <sebastian6551@live.com>
2022-11-28 21:17:53 -0500

    In course and passengers on the driver's homepage

--- src/components/PrincipalDriver.jsx
+++ src/components/PrincipalDriver.jsx
@@ -1,12 +1,71 @@
 import './styles/principalDriver.css';
 import './styles/principalBidder.css';
 import AuthContext from '../services/AuthContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppBarComponent } from './AppBarComponent';
 import logOutArrow from '../assets/icons/logOutArrow.png';
 
 export const PrincipalDriver = () => {
 	const { logout } = useContext(AuthContext);
+
+	// If there is no trip in progress when calling the DB, a message is displayed
+	const tripInCourse = dataCall => {
+		if (dataCall) {
+			return (
+				<div>
+					<PrincipalDriverBody />
+				</div>
+			);
+		} else {
+			return (
+				<div className='inCourseCaptionContainer'>
+					<h1 className='noTripInCourseCaption'>No hay viajes en curso.</h1>
+				</div>
+			);
+		}
+	};
+
+	const PrincipalDriverBody = () => {
+		// They will be modified according to the DB information.
+		const [dayCaption] = useState('Lunes');
+		const [hourCaption] = useState('6:30 am');
+		const [departureCaption] = useState('Centro');
+		const [arrivalCaption] = useState('Univalle');
+		const [passengerName] = useState('Victor');
+		const [pickupPlace] = useState('Cra 80 Cll 13');
+
+		return (
+			<div>
+				<div className='inCourseContainer'>
+					<div className='firstLineInCourse'>
+						<label className='dayCaptionInCourse'>{dayCaption}</label>
+						<label className='hourCaptionInCourse'>{hourCaption}</label>
+					</div>
+					<label className='departureArrivalCaptions'>
+						Partida: {departureCaption}
+					</label>
+					<label className='departureArrivalCaptions'>
+						Llegada: {arrivalCaption}
+					</label>
+				</div>
+				<div className='passengersCaptionContainer'>
+					<h1 className='passengersCaption'>Pasajeros</h1>
+				</div>
+				<div className='passengersBodyContainer'>
+					<div className='passengersContainer'>
+						<label className='passengerNameCaption'>{passengerName}</label>
+						<label className='pickupCaption'>Recoger en: {pickupPlace}</label>
+					</div>
+					<div className='space9px'></div>
+					<div className='passengersContainer'>
+						<label className='passengerNameCaption'>{passengerName}</label>
+						<label className='pickupCaption'>Recoger en: {pickupPlace}</label>
+					</div>
+				</div>
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<div className='space2vh'></div>
@@ -22,10 +81,13 @@ export const PrincipalDriver = () => {
 			</span>
 			<div className='space9px'></div>
 			<button className='searchTravelButton' title='Crear viaje' type='button'>
-				Crear viaje +
+				Nuevo viaje +
 			</button>
 			<div className='space9px'></div>
-			<div className='inCourseContainer'></div>
+			<div className='inCourseCaptionContainer'>
+				<h1 className='inCourseCaption'>En curso</h1>
+			</div>
+			{tripInCourse(true)}
 			<div className='space9px'></div>
 			<div className='appBarPosition'>
 				<AppBarComponent></AppBarComponent>
