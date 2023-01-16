93ae4533d4e5d027d9b0e9d25ff06563fffad595
chris_Sanchez098 <christian.valenzuela@correounivalle.edu.co>
2023-01-15 22:10:53 -0500

    Search trip funcionality an see details in card
    Added a datetimepicker in the SearchTrip interface, methods for the fecht data  and display of the trips obtained.
    The eye icon was added to be able to show the modal with all the details of the trip
    handledUpcoming method in barComponnet update to be able to redirect according to the user
    
    	modified:   src/components/AppBarComponent.jsx
    	modified:   src/components/CardComponent.jsx
    	deleted:    src/components/CardDetailRider.jsx
    	modified:   src/components/styles/searchTripRider.css
    	modified:   src/pages/SearchTripRider.jsx

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,115 +1,163 @@
 import { Typography, Grid, Paper, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import { DetailTripRider } from '../pages/DetailTripRider';
 
 export const CardComponent = props => {
+	const [open, setOpen] = useState(false);
+
 	const handleDelete = event => {
 		event.preventDefault();
-		const temp = props.handleDelete;
-		temp(props.id);
+		const del = props.handleDelete;
+		del(props.id);
+	};
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
 	};
 
 	return (
-		<Paper
-			sx={{
-				p: 2,
-				margin: 'auto',
-				width: 300,
-				background: '#FDBA43',
-				borderRadius: 5,
-			}}
-		>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'flex-end',
+		<div>
+			<Paper
+				sx={{
+					p: 1,
+					margin: 'auto',
+					width: 300,
+					background: '#FDBA43',
+					borderRadius: 5,
 				}}
 			>
-				<IconButton onClick={handleDelete} style={{ padding: '0' }}>
-					<DeleteIcon fontSize='small' />
-				</IconButton>
-			</div>
-			<Grid container spacing={2}>
-				<Grid item xs={12} sm container>
-					<Grid item xs container direction='column' spacing={2}>
-						<Grid item xs textAlign={'left'}>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'flex-end',
+					}}
+				>
+					{props.typeUser === 'bidder' && (
+						<IconButton onClick={handleDelete} style={{ padding: '0' }}>
+							<DeleteIcon fontSize='small' />
+						</IconButton>
+					)}
+				</div>
+				<Grid container spacing={1}>
+					<Grid item xs={12} sm container>
+						<Grid item xs container direction='column' spacing={2}>
+							<Grid item xs textAlign={'left'}>
+								<Typography
+									textAlign='left'
+									sx={{
+										fontFamily: 'jost',
+										fontStyle: 'normal',
+										fontSize: 15,
+										fontWeight: 700,
+									}}
+								>
+									{props.day}
+								</Typography>
+								<Typography
+									textAlign='left'
+									sx={{
+										fontFamily: 'jost',
+										fontStyle: 'normal',
+										fontSize: 15,
+										fontWeight: 400,
+										left: 34,
+									}}
+								>
+									Partida:
+								</Typography>
+								<Typography
+									textAlign='left'
+									sx={{
+										fontFamily: 'jost',
+										fontStyle: 'normal',
+										fontSize: 15,
+										fontWeight: 400,
+									}}
+								>
+									LLegada:
+								</Typography>
+							</Grid>
+						</Grid>
+
+						<Grid item textAlign={'left'}>
 							<Typography
-								textAlign='left'
+								textAlign='right'
 								sx={{
 									fontFamily: 'jost',
 									fontStyle: 'normal',
-									fontSize: 27,
+									fontSize: 18,
 									fontWeight: 700,
 								}}
 							>
-								{props.day}
+								{props.hour}
 							</Typography>
 							<Typography
-								textAlign='left'
+								textAlign='right'
 								sx={{
 									fontFamily: 'jost',
 									fontStyle: 'normal',
-									fontSize: 20,
+									fontSize: 15,
 									fontWeight: 400,
-									left: 34,
+									color: '#FFFFFF',
 								}}
 							>
-								Partida:
+								{props.startingPoint}
 							</Typography>
+
 							<Typography
-								textAlign='left'
+								textAlign='right'
 								sx={{
 									fontFamily: 'jost',
 									fontStyle: 'normal',
-									fontSize: 20,
+									fontSize: 15,
 									fontWeight: 400,
+									color: '#FFFFFF',
 								}}
 							>
-								LLegada:
+								{props.arrivalPoint}
 							</Typography>
 						</Grid>
 					</Grid>
-
-					<Grid item textAlign={'left'}>
-						<Typography
-							textAlign='right'
-							sx={{
-								fontFamily: 'jost',
-								fontStyle: 'normal',
-								fontSize: 25,
-								fontWeight: 700,
-							}}
-						>
-							{props.hour}
-						</Typography>
-						<Typography
-							textAlign='right'
-							sx={{
-								fontFamily: 'jost',
-								fontStyle: 'normal',
-								fontSize: 20,
-								fontWeight: 400,
-								color: '#FFFFFF',
-							}}
-						>
-							{props.startingPoint}
-						</Typography>
-
-						<Typography
-							textAlign='right'
-							sx={{
-								fontFamily: 'jost',
-								fontStyle: 'normal',
-								fontSize: 20,
-								fontWeight: 400,
-								color: '#FFFFFF',
-							}}
-						>
-							{props.arrivalPoint}
-						</Typography>
-					</Grid>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+						}}
+					>
+						<IconButton onClick={handleOpen} style={{ padding: '3' }}>
+							<RemoveRedEyeIcon fontSize='small' />
+						</IconButton>
+					</div>
 				</Grid>
-			</Grid>
-		</Paper>
+			</Paper>
+			<Dialog open={open} onClose={handleClose} fullScreen>
+				<DialogContent>
+					<DetailTripRider
+						day={props.day}
+						hour={props.hour}
+						startingPoint={props.startingPoint}
+						arrivalPoint={props.arrivalPoint}
+						rate={props.other ? props.other[0] : ''}
+						description={props.other ? props.other[1] : ''}
+						typeUser={props.typeUser}
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Cerrar</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
 	);
 };
