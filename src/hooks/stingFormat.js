0aa0ed5b45881308be2f84cee8063cfeed2df900
chris_Sanchez098 <christian.valenzuela@correounivalle.edu.co>
2023-01-15 22:02:47 -0500

    Added documentatios to hooks 	modified:   package-lock.json 	modified:   package.json 	modified:   src/hooks/bidder.services.js 	new file:   src/hooks/jsonFormat.js 	new file:   src/hooks/rider.services.js 	modified:   src/hooks/stingFormat.js

--- src/hooks/stingFormat.js
+++ src/hooks/stingFormat.js
@@ -1,5 +1,5 @@
 /**
- * Inserts a string at a given position, modifying the original string
+ * Insert a string in a position of another string
  * @param {string} str string to modified
  * @param {number} index position to insert
  * @param {string} replacement string to insert
@@ -15,14 +15,21 @@ const replaceAt = (str, index, replacement) => {
 };
 
 /**
- * "2022-12-31T16:50:00.000Z"
+ * Get the hour from a date in format ISO8601
  * @param {string} dateStr date in format ISO8601
  * @returns {string} hour
+ * @example getHour('2020-05-05T12:00:00.000Z') -> '12:00'
  */
 const getHour = dateStr => {
 	return dateStr.substring(11, 16);
 };
 
+/**
+ * Set the day of the week
+ * @param {int} num
+ * @returns string
+ * @example setDay(1) -> 'Lunes'
+ */
 const setDay = num => {
 	switch (num) {
 		case 1:
@@ -42,12 +49,24 @@ const setDay = num => {
 	}
 };
 
+/**
+ * Set the date in format 'dd/mm'
+ * @param {string} str date in format ISO8601
+ * @returns {string} date in format 'mm/dd'
+ * @example setTime('Fri Jan 20 2023 21:49:16 GMT-0500') -> 'Jan 20'
+ *
+ * */
 const setTime = str => {
-	const date = new Date(str);
-	const other = date.toDateString();
-	return other.substring(3, 11);
+	const date = new Date(str).toDateString();
+	return date.substring(3, 11);
 };
 
+/**
+ * Split a string by comma (,) and return the first two elements
+ * @param {string} str
+ * @returns {string} meet point
+ * @example setMeetPoint('Carrera 26c 2 #77-43, Cali, Valle del Cauca, Colombia) -> 'Carrera 26c 2 #77-43, Cali'
+ * */
 const setMeetPoint = str => {
 	const arr = str.split(',');
 	return arr[0] + ',' + arr[1];
